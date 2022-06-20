43d485addc7affae2addb0fcbbadec4eeec3d075
Breno Nascimento <satsukebare23@gmail.com>
2022-06-19 23:10:48 -0300

    criando componentes de CardMessage

---
+++ src/components/CardExercices/CardMessageComponent/CardMessage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class CardMessage extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          dawpodwa
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default CardMessage;
---
+++ src/components/CardExercices/CardMessageComponent/CardMessageStyle.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+
+export const MessageCardContainer = styled.View`
+  padding: 15px;
+  width: 100%;
+  height: auto;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 20px;
+  background-color: rgba(128,128,128,0.3);
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+`;
+
+export const ProgressText = styled.Text`
+  font-size: 18px;
+  font-weight: 600;
+  color: white;
+`;
+
+export const ContainerRow = styled.View`
+  flex-direction: row;
+  align-self: flex-start;
+`;
+
+export const TextMessage = styled.Text `
+  color: #ddd;
+  align-self: flex-start;
+  margin-top: 10px;
+`
+
+
